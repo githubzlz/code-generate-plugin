c94a4702b78ddcac9b655b59a223206ed431fef5
GitHub Action <action@github.com>
2021-12-24 08:41:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/githubzlz/codegenerateplugin/MyBundle.kt
+++ src/main/kotlin/com/github/githubzlz/codegenerateplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.githubzlz.codegenerateplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/githubzlz/codegenerateplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.githubzlz.codegenerateplugin.services
+
+import com.github.githubzlz.codegenerateplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
